247277ab543fa25bcd8a3e552a98000d225d030a
F9329132 Thiago de Oliveira Almeida <thiago.sse@bb.com.br>
2022-07-13 19:46:14 -0300

    a

---
+++ src/pages/atividades/a.js
@@ -0,0 +1,98 @@
+const x = 
+{
+    "Human":
+    [
+        {
+            "id":1,
+            "name":"Rick Sanchez",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            "episodes":51
+        },
+        {
+            "id":2,
+            "name":"Morty Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+            "episodes":51
+        },
+        {
+            "id":3,
+            "name":"Summer Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Female",
+            "image":"https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+            "episodes":42
+        },
+        {
+            "id":4,
+            "name":"Beth Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Female",
+            "image":"https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+            "episodes":42
+        },
+        {
+            "id":5,
+            "name":"Jerry Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+            "episodes":39
+        }
+    ],
+    "robot": [
+        {
+            "id":1,
+            "name":"Rick Sanchez",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            "episodes":51
+        },
+        {
+            "id":2,
+            "name":"Morty Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+            "episodes":51
+        },
+        {
+            "id":3,
+            "name":"Summer Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Female",
+            "image":"https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+            "episodes":42
+        },
+        {
+            "id":4,
+            "name":"Beth Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Female",
+            "image":"https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+            "episodes":42
+        },
+        {
+            "id":5,
+            "name":"Jerry Smith",
+            "status":"Alive",
+            "species":"Human",
+            "gender":"Male",
+            "image":"https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+            "episodes":39
+        }
+    ],  
+}
